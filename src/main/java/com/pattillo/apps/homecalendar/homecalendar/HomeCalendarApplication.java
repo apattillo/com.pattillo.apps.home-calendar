6556280b1ee21cfe4921108cabab65a992e740a4
Aaron Pattillo <alpattillo@hotmail.com>
2018-05-24 20:33:09 -0500

    Initial commit of spring-boot generated project

---
+++ src/main/java/com/pattillo/apps/homecalendar/homecalendar/HomeCalendarApplication.java
@@ -0,0 +1,12 @@
+package com.pattillo.apps.homecalendar.homecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeCalendarApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pattillo/apps/homecalendar/homecalendar/HomeCalendarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pattillo.apps.homecalendar.homecalendar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeCalendarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
